d0752cc2834ccd564a55489a19f67d5f2c39f285
Washington <68349833+washingtongomes@users.noreply.github.com>
2022-03-26 16:28:55 -0300

    Add files via upload

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,24 @@
+import "./styles.css";
+import Images from "./Images/Washington.png";
+function Header() {
+  return (
+    <header id="header">
+      <nav>
+        <span className="nav-brand">
+          <span>Let's</span>DoIt
+        </span>
+
+        <div className="profile">
+          <img src={Images}  alt="Washington - Imagem de Perfil" />
+
+          <div>
+            <p className="name">Washington Gomes</p>
+            <p className="welcome-message">Bem-vindo(a) a este site</p>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Inspiration/index.jsx
@@ -0,0 +1,33 @@
+import "./styles.css";
+
+export function Inspiration() {
+  return (
+    <section id="inspiration">
+      <h2>Para inspirar</h2>
+
+      <div className="inspiration-cards">
+        <div className="card">
+          <p className="card-content">
+            <strong>Bem-vindo! </strong>
+          </p>
+
+
+          <p className="card-content">
+            Organizando suas tarefas
+          </p>
+
+        </div>
+        <div className="card">
+          <p className="card-content">
+
+            " A organização de tarefas se faz necessário em nossas vidas. Porém, gerenciar o tempo é sabedoria de poucos. "
+          </p>
+
+          <p className="card-citation">
+            <strong> Washington</strong>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
