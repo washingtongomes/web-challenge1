8acd85ffcb3c02f76787d716bfd017d9c14b8c69
Washington <68349833+washingtongomes@users.noreply.github.com>
2022-06-16 19:21:01 -0300

    Retirada Components aPP

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 
 import Header from "./components/Header";
 import { Inspiration } from "./components/Inspiration";
-import {ConsultaAppLoja} from "./components/components";
 
 import "./styles/global.css";
 import "./styles/App.css";
@@ -69,7 +68,6 @@ function App() {
   return (
     <>
       <Header />
-      <ConsultaAppLoja />
       <Inspiration />
 
       <section id="tarefas">
