32aa74397e7264a07cf673f2bb1ffa9b8555308f
Washington <68349833+washingtongomes@users.noreply.github.com>
2022-06-17 02:09:54 -0300

    Atualização com iframe

--- src/components/AppConsulta/index.jsx
+++ src/components/AppConsulta/index.jsx
@@ -1,12 +1,12 @@
 //import "./styles.css";
 
 export function AppConsulta(){
-    retirn (
-        <>
+    return (
+        <div>
         <section>
         <iframe title="Consulta_Lojas_TI" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiNTI1NWIzYTEtNmE2Yi00OGM1LWI5NWItMDQ2NDZlNmExZWE1IiwidCI6ImFiODBjOTNmLTgwYmEtNDVhNC04MTIwLWEwOTYwNWU4ZjgwMCJ9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
         </section>
-        </>
+        </div>
     )
 }
 
